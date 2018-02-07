93666b16c5b96f8b71929ae44904f9f2d4b82682
typescriptstudies <typescriptstudies@gmail.com>
2018-02-07 13:04:44 +0100

    Add api to js commit

--- c.bat
+++ c.bat
@@ -3,11 +3,13 @@ git add -A .
 git reset -- server.js
 git reset -- scheduler.js
 git reset -- mongo.js
+git reset -- api.js
 git reset -- client/builder.js
 git commit -m "%*"
 
 git add -- server.js
 git add -- scheduler.js
 git add -- mongo.js
+git add -- api.js
 git add -- client/builder.js
 git commit -m "%* js"
